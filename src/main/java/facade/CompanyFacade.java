1793de44ab60896e247ca0306ab5e14dce825f12
DELL-E4310\Mathias <dudi.achmed@yahoo.dk>
2016-10-03 10:54:45 +0200

    starter på facader

---
+++ src/main/java/facade/CompanyFacade.java
@@ -0,0 +1,16 @@
+package facade;
+
+//@author Mathias
+
+import entity.Company;
+import static entity.Company_.cvr;
+
+
+public class CompanyFacade {
+
+  Company getCompany(int cvr)
+  {
+    
+  }
+  
+}
---
+++ src/main/java/facade/PersonFacade.java
@@ -0,0 +1,26 @@
+package facade;
+
+//@author Mathias
+
+import entity.Person;
+import java.util.List;
+
+public class PersonFacade
+{
+
+  Person getPerson(int id)
+  {
+
+  }
+
+  List<Person> getPersons()
+  {
+
+  }
+
+  List<Person> getPersons(int zipCode)
+  {
+
+  }
+
+}
