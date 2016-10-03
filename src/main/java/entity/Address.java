83848686864529e7ad89bb122e1dce8afddcd87c
DELL-E4310\Mathias <dudi.achmed@yahoo.dk>
2016-10-03 10:31:03 +0200

    index

--- src/main/java/entity/Address.java
+++ src/main/java/entity/Address.java
@@ -2,6 +2,69 @@ package entity;
 
 //@author Mathias
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class Address {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  int id;
+  
+  String street;
+  String additionalInfo;
+  @ManyToOne
+  private CityInfo cityInfo;
+
+  public int getId()
+  {
+    return id;
+  }
+
+  public void setId(int id)
+  {
+    this.id = id;
+  }
+
+  public String getStreet()
+  {
+    return street;
+  }
+
+  public void setStreet(String street)
+  {
+    this.street = street;
+  }
+
+  public String getAdditionalInfo()
+  {
+    return additionalInfo;
+  }
+
+  public void setAdditionalInfo(String additionalInfo)
+  {
+    this.additionalInfo = additionalInfo;
+  }
+
+  public CityInfo getCityInfo()
+  {
+    return cityInfo;
+  }
+
+  public void setCityInfo(CityInfo cityInfo)
+  {
+    this.cityInfo = cityInfo;
+  }
+  
+  
+  
+  
+  public Address()
+  {
+    
+  }
 }
--- src/main/java/entity/CityInfo.java
+++ src/main/java/entity/CityInfo.java
@@ -1,7 +1,75 @@
 package entity;
 
 //@author Mathias
+import java.util.ArrayList;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
-public class CityInfo {
+@Entity
+public class CityInfo
+{
 
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  int id;
+
+  int zipCode;
+  String city;
+
+  public int getId()
+  {
+    return id;
+  }
+
+  public void setId(int id)
+  {
+    this.id = id;
+  }
+
+  public int getZipCode()
+  {
+    return zipCode;
+  }
+
+  public void setZipCode(int zipCode)
+  {
+    this.zipCode = zipCode;
+  }
+
+  public String getCity()
+  {
+    return city;
+  }
+
+  public void setCity(String city)
+  {
+    this.city = city;
+  }
+
+  public ArrayList<Address> getAddresses()
+  {
+    return addresses;
+  }
+
+  public void setAddresses(ArrayList<Address> addresses)
+  {
+    this.addresses = addresses;
+  }
+
+  
+  @OneToMany(mappedBy = "cityInfo")
+  private ArrayList<Address> addresses = new ArrayList();
+  
+  
+  
+  
+  
+  public CityInfo()
+  {
+
+  }
 }
