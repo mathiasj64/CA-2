21f02b15de128293f5abdb41439caa1c27347380
Philip-Pc\Philip <moldo123@gmail.com>
2016-10-03 10:42:53 +0200

    Persistence tilføjet hehe

--- src/main/java/entity/Phone.java
+++ src/main/java/entity/Phone.java
@@ -1,10 +1,12 @@
 package entity;
 
 //@author Mathias
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@Entity
 public class Phone
 {
   
